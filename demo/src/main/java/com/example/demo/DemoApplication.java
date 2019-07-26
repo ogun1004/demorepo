dfc561aaa226e5cc0f19e53aa1adbe3f0bce1b87
guny <guny@DESKTOP-BEF9951>
2019-07-26 10:00:44 +0900

    hello world!!!

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@SpringBootApplication
-@RestController
 
+@RestController
+@SpringBootApplication
 public class DemoApplication {
 
 	@RequestMapping("/")
-	public String hello() {
-		return "Hello Spring Boot";
+	String index() {
+		return "Hello World!!!";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
